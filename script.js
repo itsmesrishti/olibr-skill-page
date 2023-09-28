7b0718705ec67910068799ced9ecfeeeed5f8085
Srishti Chaudhary <131491576+itsmesrishti@users.noreply.github.com>
2023-09-28 18:40:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-// MENU (Mobile)
-// document.getElementById("open-menu-icon").addEventListener('click', function() {
-//     document.getElementById("menu-offcanvas").style.left = "0";
+const indexHeader = document.getElementById("list-header");
+const collapsibleList = document.getElementById("collapsible-list");
 
-//     document.getElementById("close-menu-icon").addEventListener('click', function() {
-//         document.getElementById("menu-offcanvas").style.left = "-100vw";
-//     });
-// });
+// open & close collapsible list
+indexHeader.addEventListener('click', function() {
+    if (collapsibleList.style.display === "none") {
+        collapsibleList.style.display = "block";
+        document.querySelector('#list-header i').classList.remove("fa-caret-down");
+		document.querySelector('#list-header i').classList.add("fa-caret-up");
+    }
+    else {
+        collapsibleList.style.display = "none";
+        document.querySelector('#list-header i').classList.remove("fa-caret-up");
+		document.querySelector('#list-header i').classList.add("fa-caret-down");
+    }
+});
 
 
-// collapsible list
-function hideList() {
-    document.getElementsByTagName('ul')[0].style.display = "none";
-    
+const addActiveClass = function(entries, observer) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            document.querySelector('.active').setAttribute('class', '');
+            var currentTopic = document.querySelector(".info-section a[href='#" + entry.target.id + "']");
+            currentTopic.classList.add('active');
+            var newHeaderContent = document.querySelector("a[href='#" + entry.target.id + "']").innerHTML;
+            if (collapsibleList.style.display === "none") {
+                indexHeader.innerHTML = newHeaderContent + "<i class='fa-solid fa-caret-down'></i>";
+            }
+            else if (collapsibleList.style.display === "block") {
+                indexHeader.innerHTML = newHeaderContent + "<i class='fa-solid fa-caret-up'></i>";
+            }
+        }
+    })
 }
 
-document.getElementById("list-header").addEventListener('click', function() {
-    document.getElementsByTagName('ul')[0].style.display = "block";
-    document.getElementById("list-header").addEventListener('click', hideList);
-});
+const options = {
+    threshold: 0.8
+};
+
+const observer = new IntersectionObserver(addActiveClass, options);
+const sections = document.querySelectorAll(".detailed-info div");
+
+sections.forEach(function(section) {
+    observer.observe(section);
+})
