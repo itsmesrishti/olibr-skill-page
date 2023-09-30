950c3eeff43f3ee2a5d270d54b0247231c0fcd2e
Srishti Chaudhary <131491576+itsmesrishti@users.noreply.github.com>
2023-09-30 08:25:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 const indexHeader = document.getElementById("list-header");
-const collapsibleList = document.getElementById("collapsible-list");
+const collapsibleListBox = document.getElementById("collapsible-list");
 
-const listContents = document.querySelectorAll("#collapsible-list li a")
+// const listContents = document.querySelectorAll(".info-section .menu a");
+const mobileList = document.querySelectorAll("#collapsible-list li a");
+const desktopList = document.querySelectorAll("#desktop-list li a");
+
 
 // open & close collapsible list
 indexHeader.onclick = function() {
 	
-    if (collapsibleList.style.display === "") {
-	// to show the list block
-        collapsibleList.style.display = "block";
-	// to change the icon accordingly
+    if (collapsibleListBox.style.display === "") {
+		// to show the list block
+        collapsibleListBox.style.display = "block";
+		// to change the icon accordingly
         document.querySelector('#list-header i').classList.remove("fa-caret-down");
-	document.querySelector('#list-header i').classList.add("fa-caret-up");
+		document.querySelector('#list-header i').classList.add("fa-caret-up");
     }
     else {
-	// to hide the list block
-        collapsibleList.style.display = "";
-	// to change the icon accordingly
+		// to hide the list block
+        collapsibleListBox.style.display = "";
+		// to change the icon accordingly
         document.querySelector('#list-header i').classList.remove("fa-caret-up");
-	document.querySelector('#list-header i').classList.add("fa-caret-down");
+		document.querySelector('#list-header i').classList.add("fa-caret-down");
     }
 	
 };
 
 // apply styles to clicked list item and remove from previous item
 // change header content depending on link clicked
-for (var i = 0; i < listContents.length; i++) {
+for (var i = 0; i < mobileList.length; i++) {
 	
-	listContents[i].addEventListener('click', function(e) {
-		
+	mobileList[i].addEventListener('click', function(e) {
+
 		// to identify the link clicked
 		var link = e.target.href;
 		var hrefLink = link.split("#")[1];
@@ -37,18 +40,39 @@ for (var i = 0; i < listContents.length; i++) {
 		document.querySelector("#collapsible-list li a.active").setAttribute('class', '');
 
 		// add style to the link user clicked
-		document.querySelector("a[href='#" + hrefLink + "']").setAttribute('class', 'active');
+		document.querySelector("#collapsible-list a[href='#" + hrefLink + "']").setAttribute('class', 'active');
 		
 		// to grab the content of the link clicked
-		var newHeaderContent = document.querySelector("a[href='#" + hrefLink + "']").innerText;
+		var newHeaderContent = document.querySelector("#collapsible-list a[href='#" + hrefLink + "']").innerText;
 
 		// to change the content of the header with the link clicked content and also the icon
 		document.querySelector("#list-header p").innerHTML = newHeaderContent;
 		document.querySelector("#list-header i").setAttribute('class', 'fa-solid fa-caret-down');
 
 		// to close the list block after user clicks on the link
-        	collapsibleList.style.display = "";
+        collapsibleListBox.style.display = "";
 
 	});
-					 
 }
+
+
+// for desktop content list
+for (var i = 0; i < desktopList.length; i++) {
+	desktopList[i].addEventListener('click', function(e) {
+
+		// to identify the link clicked
+		var link = e.target.href;
+		var hrefLink = link.split("#")[1];
+
+		// remove style from previously selected link
+		document.querySelector("#desktop-list li a.active").setAttribute('class', '');
+
+		// add style to the link user clicked
+		document.querySelector("#desktop-list a[href='#" + hrefLink + "']").setAttribute('class', 'active');
+
+
+	});
+}
+
+
+// scroll spy
